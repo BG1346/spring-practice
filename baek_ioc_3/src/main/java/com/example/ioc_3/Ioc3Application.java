21306418c320a77b3adb1a135e844c4cafbbaeb8
bg <bg1346@naver.com>
2019-10-25 15:59:09 +0900

    directory hierachy change

---
+++ baek_ioc_3/src/main/java/com/example/ioc_3/Ioc3Application.java
@@ -0,0 +1,13 @@
+package com.example.ioc_3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ioc3Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Ioc3Application.class, args);
+    }
+
+}
