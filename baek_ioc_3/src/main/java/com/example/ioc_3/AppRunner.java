2c395d235f955efa6e6695b3e852c0976f253f31
bg <bg1346@naver.com>
2019-10-28 17:13:06 +0900

    use prototype scope and covered with proxymode every singletone scope bean can have prototype scope beans, but these are used for singletone scope. proxymode can cover prototype scope bean and injected instead prototype scope bean which proxy bean cover, so singletone scope bean can have prototype scope bean and not using catalina.core, context.ApplicationContext

--- baek_ioc_3/src/main/java/com/example/ioc_3/AppRunner.java
+++ baek_ioc_3/src/main/java/com/example/ioc_3/AppRunner.java
@@ -3,20 +3,31 @@ package com.example.ioc_3;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    Single single;
-
-    @Autowired
-    Proto proto;
+    ApplicationContext applicationContext;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(proto);
-        System.out.println(single.getProto());
+
+
+        System.out.println("proto");
+        System.out.println(applicationContext.getBean(Proto.class));
+        System.out.println(applicationContext.getBean(Proto.class));
+        System.out.println(applicationContext.getBean(Proto.class));
+        System.out.println("single");
+        System.out.println(applicationContext.getBean(Single.class));
+        System.out.println(applicationContext.getBean(Single.class));
+        System.out.println(applicationContext.getBean(Single.class));
+        System.out.println("single's prototype");
+        System.out.println(applicationContext.getBean(Single.class).getProto());
+        System.out.println(applicationContext.getBean(Single.class).getProto());
+        System.out.println(applicationContext.getBean(Single.class).getProto());
     }
 }
--- baek_ioc_3/src/main/java/com/example/ioc_3/Single.java
+++ baek_ioc_3/src/main/java/com/example/ioc_3/Single.java
@@ -1,12 +1,16 @@
 package com.example.ioc_3;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Single {
     @Autowired
     private Proto proto;
+
+//    @Autowired
+//    ApplicationContext applicationContext;
 
     public Proto getProto() {
         return proto;
