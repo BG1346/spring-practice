40b8475ba19ae89e14044037cd0d4550cba76b25
bg <bg1346@naver.com>
2019-10-28 17:36:15 +0900

    final revised

--- baek_ioc_3/src/main/java/com/example/ioc_3/AppRunner.java
+++ baek_ioc_3/src/main/java/com/example/ioc_3/AppRunner.java
@@ -1,13 +1,10 @@
 package com.example.ioc_3;
 
-import org.apache.catalina.core.ApplicationContext;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-<<<<<<< HEAD
 import org.springframework.context.ApplicationContext;
-=======
->>>>>>> 6529dc3b388543dff4654b7cb19445d635b053a8
+
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.stereotype.Component;
 
