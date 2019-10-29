9bf46e9164681bfcf04c3d967405f08c7f2af6f4
bg <bg1346@naver.com>
2019-10-29 18:19:17 +0900

    section ioc-8 eventpublisher using async, order, and default event

--- baek_ioc_3/src/main/java/com/example/ioc_3/AppRunner.java
+++ baek_ioc_3/src/main/java/com/example/ioc_3/AppRunner.java
@@ -9,11 +9,13 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.MessageSource;
 import org.springframework.core.env.Environment;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
 
 import java.util.Locale;
 
 @Component
+@EnableAsync
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
