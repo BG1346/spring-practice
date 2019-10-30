31bc2283ba6c768d34ceeca84ecbf195778be7c1
bg <bg1346@naver.com>
2019-10-30 11:06:41 +0900

    section validation, primative event validation

--- baek_ioc_3/src/main/java/com/example/ioc_3/AppRunner.java
+++ baek_ioc_3/src/main/java/com/example/ioc_3/AppRunner.java
@@ -8,10 +8,16 @@ import org.springframework.context.ApplicationContext;
 
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.context.MessageSource;
+import org.springframework.context.ResourceLoaderAware;
 import org.springframework.core.env.Environment;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.ResourceLoader;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.stereotype.Component;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.Errors;
 
+import java.util.Arrays;
 import java.util.Locale;
 
 @Component
@@ -19,11 +25,21 @@ import java.util.Locale;
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ApplicationEventPublisher applicationContext;
+    ResourceLoader resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        applicationContext.publishEvent(new MyEvent(this, 1000));
-//        applicationContext.publishEvent(new MyEvent(1000));
+        Event event = new Event();
+        EventValidator eventValidator = new EventValidator();
+        Errors errors = new BeanPropertyBindingResult(event,  "event");
+
+        eventValidator.validate(event, errors);
+        System.out.println(errors.hasErrors());
+
+        errors.getAllErrors().forEach(e -> {
+            System.out.println("==========error code==========");
+            Arrays.stream(e.getCodes()).forEach(System.out::println);
+            System.out.println(e.getDefaultMessage());
+        });
     }
 }
