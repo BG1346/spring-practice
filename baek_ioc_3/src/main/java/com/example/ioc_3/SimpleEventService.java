5e580f3c58cd22b729be6f4097ed107f42002e4c
bg <bg1346@naver.com>
2019-10-31 14:54:21 +0900

    section aop-2 implement event after adopting proxybean

--- baek_ioc_3/src/main/java/com/example/ioc_3/SimpleEventService.java
+++ baek_ioc_3/src/main/java/com/example/ioc_3/SimpleEventService.java
@@ -8,27 +8,22 @@ public class SimpleEventService implements  EventService {
 
     @Override
     public void createEvent() {
-        long begin = System.currentTimeMillis();
         try {
             Thread.sleep(1000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println("create event");
-        System.out.println(System.currentTimeMillis() - begin);
     }
 
     @Override
     public void publishEvent() {
-        long begin = System.currentTimeMillis();
-
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
         System.out.println("publish event");
-        System.out.println(System.currentTimeMillis() - begin);
     }
 
     @Override
