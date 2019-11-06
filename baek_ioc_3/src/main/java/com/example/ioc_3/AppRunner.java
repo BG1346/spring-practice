af85406f6cae10d45fc5bdbf957bc3ebbe564b4a
bg <bg1346@naver.com>
2019-11-06 15:10:33 +0900

    use applicationlistener

--- baek_ioc_3/src/main/java/com/example/ioc_3/AppRunner.java
+++ baek_ioc_3/src/main/java/com/example/ioc_3/AppRunner.java
@@ -4,7 +4,6 @@ package com.example.ioc_3;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.SpringApplication;
 
 import org.springframework.stereotype.Component;
 
---
+++ bo/src/main/java/com/example/Application.java
@@ -0,0 +1,20 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class Application {
+
+
+    public static void main(String[] args) {
+//        SpringApplication springApplication = new SpringApplication(SpringInitApplication.c);
+        SpringApplication springApplication = new SpringApplication(Application.class);
+        springApplication.addListeners(new SampleListener());
+        springApplication.run(args);
+    }
+}
