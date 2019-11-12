c66d1edc4f064db59bb2640a8561f4d2edb0f22f
bg <bg1346@naver.com>
2019-11-12 16:13:49 +0900

    use test by mocking

--- baek_test/src/main/java/com/example/demo/sample/SampleController.java
+++ baek_test/src/main/java/com/example/demo/sample/SampleController.java
@@ -1,17 +1,14 @@
-package com.example.sample;
+package com.example.demo.sample;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 @RestController
 public class SampleController {
 
     @Autowired
     private SampleService sampleService;
-
-
 
     @GetMapping("/hello")
     public String hello(){
--- baek_test/src/test/java/com/example/demo/sample/SampleControllerTest.java
+++ baek_test/src/test/java/com/example/demo/sample/SampleControllerTest.java
@@ -1,41 +1,45 @@
-package com.example.sample;
+package com.example.demo.sample;
 
+import org.assertj.core.api.Assertions;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.web.client.RestTemplate;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class SampleControllerTests {
 
-//    @Autowired
-//    MockMvc mockMvc;
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.MOCK)
+@AutoConfigureMockMvc
+public class SampleControllerTest {
+
     @Autowired
-    TestRestTemplate testRestTemplate;
+    MockMvc mockMvc;
+
+    @MockBean
+    SampleService sampleService;
 
     @Test
     public void hello() throws Exception {
-//        mockMvc.perform(get("/hello"))
-//                .andExpect(status().isOk())
-//                .andExpect(content().string("hello keesin"))
-//                .andDo(print());
+        when(sampleService.getName()).thenReturn("mock");
 
-        String result = testRestTemplate.getForObject("/hello", String.class);
-        assertThat(result).isEqualTo("hello keesin");
-
+        mockMvc.perform(get("/hello"))
+                .andExpect(status().isOk())
+                .andExpect(content().string("hello mock"))
+                .andDo(print());
     }
-
 
 }
